02eeef46df1cf62e1d74b32853b3983a882e2559
LevshinaA <113028419+LevshinaA@users.noreply.github.com>
2022-09-26 15:56:44 +0300

    меню

---
+++ laba/laba/laba.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Myclass
+{
+
+};
+
+
+int main()
+{
+    bool povtor = 1;
+    int number = 0;
+    vector <Myclass> v={Myclass};
+
+    setlocale(LC_ALL, "Russian");
+    while (povtor == true)
+    {
+        cout << "1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов\n4. Редактировать трубу\n5. Редактировать КС\n6. Сохранить\n7. Загрузить\n0. Выход\n";
+        cin >> number;
+        if (number == 0){
+            break;
+        }
+        if (number > 0 && number <= 7){
+            cout << "function\n";
+        }
+        else {
+            cout << "Ошибка!\n";
+        }
+    }
+}
+
