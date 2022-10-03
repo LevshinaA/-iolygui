6696451d36475652989f06eef78b40278c9d0afa
LevshinaA <113028419+LevshinaA@users.noreply.github.com>
2022-10-03 20:35:23 +0300

    tube

--- laba/laba/laba.cpp
+++ laba/laba/laba.cpp
@@ -1,34 +1,54 @@
-﻿
-
+﻿#include <vector>
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
-struct Myclass
+
+class Tube
 {
+public:
+    void addparams() {
+        cout << "Введите длинну";
+        cin >> length;
+        cout << "Введите диаетр";
+        cin >> diameter;
+        cout << "Введите описание";
+        cin >> feature;
+    }
+    void show() {
+        cout << "Длина: " << length << "   Диаметр : " << diameter << "   Признак : " << feature;
+
+    }
+
+private:
+    int length, diameter, i;
+    string feature, type = "Труба";
 
 };
 
-
 int main()
 {
-    bool povtor = 1;
-    int number = 0;
-    vector <Myclass> v={Myclass};
 
     setlocale(LC_ALL, "Russian");
-    while (povtor == true)
+
+    vector<Tube> tubebox;
+    int i;
+    int button;
+
+    while (true)
     {
         cout << "1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов\n4. Редактировать трубу\n5. Редактировать КС\n6. Сохранить\n7. Загрузить\n0. Выход\n";
-        cin >> number;
-        if (number == 0){
-            break;
+        cin >> button;
+        if (button == 1) {
+            Tube realtube;
+            realtube.addparams();
+            tubebox.push_back(realtube);
         }
-        if (number > 0 && number <= 7){
-            cout << "function\n";
-        }
-        else {
-            cout << "Ошибка!\n";
+        if (button == 3) {
+            for (int i = 0; i < tubebox.size(); i++) {
+                cout << i;
+                tubebox[i].show();
+            }
         }
     }
+    return 0;
 }
-
